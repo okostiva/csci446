5634ed46dea53fb0223fbbdca877b19801bb122e
Orion Kostival <okostiva@mines.edu>
2013-02-27 18:59:22 -0700

    Added image and price unit tests

--- project04/test/unit/product_test.rb
+++ project04/test/unit/product_test.rb
@@ -9,4 +9,41 @@ class ProductTest < ActiveSupport::TestCase
 		assert product.errors[:price].any?
 		assert product.errors[:image_url].any?
 	end
+
+	test "product price must be positive" do
+		product = Product.new(title:		"My First Book",
+							  description: 	"A new book",
+							  image_url: 	"book.png")
+		product.price = -1
+		assert product.invalid?
+		assert_equal ["must be greater than or equal to 0.01"], product.errors[:price]
+
+		product.price = 0
+		assert product.invalid?
+		assert_equal ["must be greater than or equal to 0.01"], product.errors[:price]
+
+		product.price = 1
+		assert product.valid?
+	end
+
+	def new_product(image_url)
+		Product.new(title:			"New Product Title",
+					description:	"Some description",
+					price:			1,
+					image_url: 		image_url)
+	end
+
+	test "image url properly formatted" do
+		ok_images = %w{ fred.gif fred.jpg fred.png FRED.jpg FRED.JPG http://a.b.c.d.e/x/y/z/fred.gif }
+	
+		bad_images = %w{ fred.doc fred.gif/extra fred.gif.extra }
+
+		ok_images.each do |image_name|
+			assert new_product(image_name).valid?, "#{image_name} should not have been invalid"
+		end
+
+		bad_images.each do |image_name|
+			assert new_product(image_name).invalid?, "#{image_name} should not have been valid"
+		end
+	end
 end
